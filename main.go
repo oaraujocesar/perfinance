3f92eeda026c24f634056f3a470c6c56ba60e5f8
César O. Araújo <oaraujocesar@gmail.com>
2023-01-18 20:27:50 -0300

    add first routes

--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/oaraujocesar/perfinance/controller"
+	"github.com/oaraujocesar/perfinance/database"
 )
 
 func main() {
-	godotenv.Load(".env")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	database.Connect()
 
 	router := gin.Default()
+
+	router.SetTrustedProxies(nil)
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
+	router.POST("/entries", controller.CreateEntry)
+
+	router.POST("/categories", controller.CreateCategory)
+
+	router.POST("/types", controller.CreateType)
 
 	router.Run()
 }
