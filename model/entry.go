987d29d9b7f066f5f7339f5044cfcc540b571312
César O. Araújo <oaraujocesar@gmail.com>
2023-01-18 20:27:10 -0300

    create inital models

---
+++ model/entry.go
@@ -0,0 +1,14 @@
+package model
+
+import "gorm.io/gorm"
+
+type Entry struct {
+	gorm.Model
+
+	Title      string   `json:"title"`
+	Amount     uint     `json:"amount"`
+	TypeID     uint     `json:"typeID"`
+	Type       Type     `json:"type"`
+	CategoryID uint     `json:"categoryID"`
+	Category   Category `json:"category"`
+}
